657cafbaed0e4a95dd363fe004175f04ab047ace
John O'Brien <jobrieniii@540.co>
2018-01-25 01:50:29 -0500

    wip

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 var api = require('./routes/api');
-var accounts = require('./routes/accounts');
-var transactions = require('./routes/transactions');
 
 var app = express();
 
@@ -25,11 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/api', api);
-app.use('/users', users);
-app.use('/accounts', accounts);
-app.use('/transactions', transactions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
